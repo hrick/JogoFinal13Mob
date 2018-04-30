41cb82280d34c516cd8855f606b0e56286339677
hrick <henrique963@hotmail.com>
2018-04-29 23:50:35 -0300

    Your Message

---
+++ Assets/scripts/GerarAboboras.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerarAboboras : MonoBehaviour {
+
+	public float limiteEsquerdo, limiteDireito, limiteFrontal, limiteTraseiro;
+	public float frequencia;
+	public GameObject aboboraPrefab;
+
+	IEnumerator Start () {
+		
+		yield return new WaitForSeconds (Random.Range (3.0f, frequencia));
+		Vector3 posicao;
+		posicao.x = Random.Range (limiteEsquerdo, limiteDireito);
+		posicao.y = transform.position.y;
+		posicao.z = Random.Range (limiteFrontal, limiteTraseiro);
+		Instantiate (aboboraPrefab, posicao, transform.rotation);
+		StartCoroutine (Start ());
+		
+	}	
+
+}
---
+++ Assets/scripts/PersonagemScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemScript : MonoBehaviour {
+
+	public GameObject personagem;
+	public float velocidade;
+
+	Vector3 novaPosicao;
+	Animation ani;
+
+	void OnCollisionEnter(Collision c){
+		if (c.gameObject.tag == "item") {
+			Destroy (c.gameObject);
+		}
+	}
+
+
+	void Start () {
+		// Captura a posicao inicial do personagem
+		novaPosicao = transform.position;
+		ani = personagem.GetComponent<Animation> ();
+
+		// Define a animacao inicial do personagem
+		ani.CrossFade("idle");
+	}
+
+	void Update () {
+	
+		// Touch ou clique do mouse
+		if (Input.GetButton ("Fire1") ) {			
+			// Transforma o clique na tela em coordenada 3D
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			// Obtem a nova posica
+			if (Physics.Raycast (ray, out hit)) {
+				novaPosicao = hit.point;
+			}
+
+			// Move o personagem e orienta o personagem
+			transform.position = Vector3.MoveTowards (transform.position, novaPosicao, velocidade * Time.deltaTime);
+			transform.LookAt (hit.point);
+
+			// Animacao de run
+			ani.CrossFade("run");
+
+		} else {
+			// Animacao de idle
+			ani.CrossFade("idle");
+		}
+
+	}
+
+}
